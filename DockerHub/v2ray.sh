7a5bdb5c4b259d5b2552bed3f830497b834985f3
charlieethan <charlieethan.hk@gmail.com>
2020-10-10 13:56:46 +0800

    test

--- DockerHub/v2ray.sh
+++ DockerHub/v2ray.sh
@@ -2,24 +2,27 @@
 
 # Get code
 CUR=${PWD}
-TAG=$(wget -qO- https://api.github.com/repos/v2fly/v2ray-core/tags | grep 'name' | cut -d\" -f4 | head -n1)
-git clone -b ${TAG} https://github.com/v2fly/v2ray-core.git && cd v2ray-core
+TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
+git clone -b ${TAG} https://github.com/v2fly/v2ray-core.git && cp v2ray-core/release/config/*.dat ${CUR} && cp v2ray-core/release/config/config.json ${CUR}
+cd v2ray-core
 
 # Build release
 ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
 ARMS=( 6 7 )
+LDFLAGS="-s -w -buildid="
+
 for ARCH in ${ARCHS[@]}; do
     if [ "${ARCH}" = "arm" ]; then
         for ARM in ${ARMS[@]}; do
             echo "Building v2ray-${ARCH}v${ARM}"
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ray -trimpath -ldflags "-s -w" ./main
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ctl -trimpath -ldflags "-s -w" -tags confonly ./infra/control/main
+            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ray -trimpath -ldflags "${LDFLAGS}" ./main
+            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ctl -trimpath -ldflags "${LDFLAGS}" -tags confonly ./infra/control/main
             chmod +x v2ray v2ctl && tar -zvcf ${CUR}/v2ray-${ARCH}v${ARM}.tar.gz v2ray v2ctl && rm -fv v2ray v2ctl
         done
     else
         echo "Building v2ray-${ARCH}"
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ray -trimpath -ldflags "-s -w" ./main
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ctl -trimpath -ldflags "-s -w" -tags confonly ./infra/control/main
+        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ray -trimpath -ldflags "${LDFLAGS}" ./main
+        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ctl -trimpath -ldflags "${LDFLAGS}" -tags confonly ./infra/control/main
         chmod +x v2ray v2ctl && tar -zvcf ${CUR}/v2ray-${ARCH}.tar.gz v2ray v2ctl && rm -fv v2ray v2ctl
     fi
 done
