e8f997935f09590f83e0f456f1b6c94b0aeb4f0b
charlieethan <charlieethan.hk@gmail.com>
2020-10-11 01:11:11 +0800

    try to fix

--- DockerHub/v2ray.sh
+++ DockerHub/v2ray.sh
@@ -1,28 +1,57 @@
-#!/bin/bash
+#!/bin/sh
 
-# Get code
-CUR=${PWD}
-TAG=$(cat ReleaseTag | head -n1)
-git clone -b ${TAG} https://github.com/v2fly/v2ray-core.git && cp v2ray-core/release/config/*.dat ${CUR} && cp v2ray-core/release/config/config.json ${CUR}
-cd v2ray-core
+# Set ARG
+PLATFORM=$1
+if [ -z "$PLATFORM" ]; then
+    ARCH="64"
+else
+    case "$PLATFORM" in
+        linux/386)
+            ARCH="32"
+            ;;
+        linux/amd64)
+            ARCH="64"
+            ;;
+        linux/arm/v6)
+            ARCH="arm32-v6"
+            ;;
+        linux/arm/v7)
+            ARCH="arm32-v7a"
+            ;;
+        linux/arm64|linux/arm64/v8)
+            ARCH="arm64-v8a"
+            ;;
+        linux/ppc64le)
+            ARCH="ppc64le"
+            ;;
+        linux/s390x)
+            ARCH="s390x"
+            ;;
+        *)
+            ARCH=""
+            ;;
+    esac
+fi
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 
-# Build release
-ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
-ARMS=( 6 7 )
-LDFLAGS="-s -w -buildid="
+# Download files
+V2RAY_FILE="v2ray-linux-${ARCH}.zip"
+echo "Downloading binary file: ${V2RAY_FILE}"
 
-for ARCH in ${ARCHS[@]}; do
-    if [ "${ARCH}" = "arm" ]; then
-        for ARM in ${ARMS[@]}; do
-            echo "Building v2ray-linux-${ARCH}v${ARM}"
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ray -trimpath -ldflags "${LDFLAGS}" ./main
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ctl -trimpath -ldflags "${LDFLAGS}" -tags confonly ./infra/control/main
-            chmod +x v2ray v2ctl && tar -zvcf ${CUR}/v2ray-${ARCH}v${ARM}.tar.gz v2ray v2ctl && rm -fv v2ray v2ctl
-        done
-    else
-        echo "Building v2ray-linux-${ARCH}"
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ray -trimpath -ldflags "${LDFLAGS}" ./main
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ctl -trimpath -ldflags "${LDFLAGS}" -tags confonly ./infra/control/main
-        chmod +x v2ray v2ctl && tar -zvcf ${CUR}/v2ray-${ARCH}.tar.gz v2ray v2ctl && rm -fv v2ray v2ctl
-    fi
-done
+TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
+wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download binary file: ${V2RAY_FILE}" && exit 1
+fi
+echo "Download binary file: ${V2RAY_FILE} completed"
+
+echo "Prepare to use"
+unzip v2ray.zip && chmod +x v2ray v2ctl
+mv v2ray /usr/bin/v2ray && mv v2ctl /usr/bin/v2ctl
+mv geoip.dat /usr/bin/geoip.dat && mv geosite.dat /usr/bin/geosite.dat
+mv config.json /etc/v2ray/config.json
+
+# Clean
+rm -rf ${PWD}/*
+echo "Done"
