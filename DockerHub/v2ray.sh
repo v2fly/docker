e458895e95e450ec71ce216e2ac71a00d5e6d15e
charlieethan <charlieethan.hk@gmail.com>
2020-10-10 17:44:19 +0800

    fix

--- DockerHub/v2ray.sh
+++ DockerHub/v2ray.sh
@@ -2,7 +2,7 @@
 
 # Get code
 CUR=${PWD}
-TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
+TAG=$(cat ReleaseTag | head -n1)
 git clone -b ${TAG} https://github.com/v2fly/v2ray-core.git && cp v2ray-core/release/config/*.dat ${CUR} && cp v2ray-core/release/config/config.json ${CUR}
 cd v2ray-core
 
@@ -14,13 +14,13 @@ LDFLAGS="-s -w -buildid="
 for ARCH in ${ARCHS[@]}; do
     if [ "${ARCH}" = "arm" ]; then
         for ARM in ${ARMS[@]}; do
-            echo "Building v2ray-${ARCH}v${ARM}"
+            echo "Building v2ray-linux-${ARCH}v${ARM}"
             env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ray -trimpath -ldflags "${LDFLAGS}" ./main
             env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${ARM} go build -o v2ctl -trimpath -ldflags "${LDFLAGS}" -tags confonly ./infra/control/main
             chmod +x v2ray v2ctl && tar -zvcf ${CUR}/v2ray-${ARCH}v${ARM}.tar.gz v2ray v2ctl && rm -fv v2ray v2ctl
         done
     else
-        echo "Building v2ray-${ARCH}"
+        echo "Building v2ray-linux-${ARCH}"
         env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ray -trimpath -ldflags "${LDFLAGS}" ./main
         env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -o v2ctl -trimpath -ldflags "${LDFLAGS}" -tags confonly ./infra/control/main
         chmod +x v2ray v2ctl && tar -zvcf ${CUR}/v2ray-${ARCH}.tar.gz v2ray v2ctl && rm -fv v2ray v2ctl
