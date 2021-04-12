61621eaa763c41a0bcf3083a0b6507c86091733e
Loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-04-12 16:07:23 +0800

    Feat: use workflow_dispatch to input build variables (#25)

--- v2ray.sh
+++ v2ray.sh
@@ -2,6 +2,7 @@
 
 # Set ARG
 PLATFORM=$1
+TAG=$2
 if [ -z "$PLATFORM" ]; then
     ARCH="64"
 else
@@ -34,7 +35,6 @@ DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
 echo "Downloading binary file: ${V2RAY_FILE}"
 echo "Downloading binary file: ${DGST_FILE}"
 
-TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
 wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
 wget -O ${PWD}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
 
