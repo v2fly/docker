68bada24e25965b910ac9568c265e11110e18938
Kslr <kslrwang@gmail.com>
2022-10-06 17:41:02 +0800

    remove v2ctl

--- v2ray.sh
+++ v2ray.sh
@@ -55,8 +55,8 @@ fi
 
 # Prepare
 echo "Prepare to use"
-unzip v2ray.zip && chmod +x v2ray v2ctl
-mv v2ray v2ctl /usr/bin/
+unzip v2ray.zip && chmod +x v2ray
+mv v2ray /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
 mv config.json /etc/v2ray/config.json
 
